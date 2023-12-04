e42eb15a4252acac3e9f440dc0367e749b384c2d
sguh0003 <sguh0003@student.monash.edu>
2023-12-04 12:32:13 +0800

    Formatted navbar to match theme.

--- src/pages/Select.js
+++ src/pages/Select.js
@@ -1,11 +1,20 @@
 const Select = () => {
     return (
         
-        <div>
-            <div class="flex h-screen">
-                
+        <div class="font-mono text-lg">
+            <nav className="bg-black text-white p-4">
+                <ul className="flex space-x-4">
+                <li><a href="http://localhost:3000/home" className="hover:text-gray-400">Home</a></li>
+                <li className="ml-auto">
+                    <a href="http://localhost:3000/" className="hover:text-gray-400">
+                    Log Out
+                    </a>
+                </li>
+                </ul>
+            </nav>
 
-                <div className="overflow-auto ring-2 ring-gray-300 w-1/5 rounded-2xl text-xl font-mono mr-4 m-20 text-center">
+            <div class="flex h-screen"> 
+                <div className="overflow-auto ring-2 ring-gray-300 w-1/5 rounded-2xl text-xl mr-4 m-20 text-center">
                     <div class="p-8 hover:bg-black hover:text-white border-b-2">
                         <button >FIT3170</button>
                     </div>
@@ -14,7 +23,7 @@ const Select = () => {
                     </div>
                 </div>
 
-                <div className="overflow-auto ring-2 ring-gray-300 w-4/5 rounded-2xl text-xl font-mono ml-4 m-20">
+                <div className="overflow-auto ring-2 ring-gray-300 w-4/5 rounded-2xl text-xl ml-4 m-20">
                     <div class="flex justify-between border-b-2">
                         <span class="m-8">Tutorial 9:00 - 11:00</span>
                         <button class="ring-2 ring-gray-300 hover:bg-gray-100 rounded-2xl float-right py-2 px-10 m-6">Select</button>
