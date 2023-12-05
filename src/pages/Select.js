24e88a55791d4d44e55601072dc72ae3e8bc2dff
Wong En Xin <ewon0024@student.monash.edu>
2023-12-05 14:26:19 +0800

    not working code database timeslot

--- src/pages/Select.js
+++ src/pages/Select.js
@@ -2,80 +2,82 @@ import React, { useRef } from "react";
 import { db } from "../firebase-handler";
 import { addDoc, collection } from "firebase/firestore";
 
-
 const Select = () => {
-    const timeslotRef = collection(db, "timeslot");
-    const [user, setUser] = useState(null);
+  const timeslotRef = useRef();
+  const timeslotCollection = collection(db, "timeslot"); 
 
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged((user) => {
-        setUser(user);
-        });
+  const handleSave = async (timeslot) => {
+    try {
+      let data = {
+        Timespan: timeslot,
+      };
 
-        return () => unsubscribe();
-    }, []);
+      await addDoc(timeslotCollection, data); 
+      console.log("Document added successfully");
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+  };
 
-    const handleTimeSlotSelection = async (timeSlot) => {
-        try {
-        if (!user) {
-            console.error("User not authenticated");
-            return;
-        }
+  return (
+    <div className="font-mono text-lg">
+      <nav className="bg-black text-white p-4">
+        <ul className="flex space-x-4">
+          <li>
+            <a href="http://localhost:3000/home" className="hover:text-gray-400">
+              Home
+            </a>
+          </li>
+          <li className="ml-auto">
+            <a href="http://localhost:3000/" className="hover:text-gray-400">
+              Log Out
+            </a>
+          </li>
+        </ul>
+      </nav>
 
-        const data = {
-            user: user.uid,
-            timeSlot: timeSlot,
-        };
-
-        const docRef = await addDoc(timeslotRef, data);
-
-        console.log("Document written with ID: ", docRef.id);
-        } catch (error) {
-        console.error("Error adding document: ", error);
-        }
-    };
-
-    return (
-        
-        <div class="font-mono text-lg">
-            <nav className="bg-black text-white p-4">
-                <ul className="flex space-x-4">
-                <li><a href="http://localhost:3000/home" className="hover:text-gray-400">Home</a></li>
-                <li className="ml-auto">
-                    <a href="http://localhost:3000/" className="hover:text-gray-400">
-                    Log Out
-                    </a>
-                </li>
-                </ul>
-            </nav>
-
-            <div class="flex h-screen"> 
-                <div className="overflow-auto ring-2 ring-gray-300 w-1/5 rounded-2xl text-xl mr-4 m-20 text-center">
-                    <div class="p-8 hover:bg-black hover:text-white border-b-2">
-                        <button >FIT3170</button>
-                    </div>
-                    <div class="p-8 hover:bg-black hover:text-white border-b-2">
-                        <button>FIT3134</button>
-                    </div>
-                </div>
-
-                <div className="overflow-auto ring-2 ring-gray-300 w-4/5 rounded-2xl text-xl ml-4 m-20">
-                    <div class="flex justify-between border-b-2">
-                        <span class="m-8">Tutorial 9:00 - 11:00</span>
-                        <button onClick={() => handleTimeSlotSelection("Tutorial 9:00 - 11:00")} class="ring-2 ring-gray-300 hover:bg-gray-100 rounded-2xl float-right py-2 px-10 m-6">Select</button>
-                    </div>
-                    <div class="flex justify-between border-b-2">
-                        <span class="m-8">Tutorial 12:00 - 14:00</span>
-                        <button class="ring-2 ring-gray-300 hover:bg-gray-100 rounded-2xl float-right py-2 px-10 m-6">Select</button>
-                    </div>
-                    <div class="flex justify-between border-b-2">
-                        <span class="m-8">Tutorial 15:00 - 17:00</span>
-                        <button class="ring-2 ring-gray-300 hover:bg-gray-100 rounded-2xl float-right py-2 px-10 m-6">Select</button>
-                    </div>
-                </div>
-            </div>
+      <div className="flex h-screen">
+        <div className="overflow-auto ring-2 ring-gray-300 w-1/5 rounded-2xl text-xl mr-4 m-20 text-center">
+          <div className="p-8 hover:bg-black hover:text-white border-b-2">
+            <button>FIT3170</button>
+          </div>
+          <div className="p-8 hover:bg-black hover:text-white border-b-2">
+            <button>FIT3134</button>
+          </div>
         </div>
-    );
+
+        <div className="overflow-auto ring-2 ring-gray-300 w-4/5 rounded-2xl text-xl ml-4 m-20">
+          <div className="flex justify-between border-b-2">
+            <span className="m-8">Tutorial 9:00 - 11:00</span>
+            <button
+              className="ring-2 ring-gray-300 hover:bg-gray-100 rounded-2xl float-right py-2 px-10 m-6"
+              onClick={() => handleSave("Tutorial 9:00 - 11:00")}
+            >
+              Select
+            </button>
+          </div>
+          <div className="flex justify-between border-b-2">
+            <span className="m-8">Tutorial 12:00 - 14:00</span>
+            <button
+              className="ring-2 ring-gray-300 hover:bg-gray-100 rounded-2xl float-right py-2 px-10 m-6"
+              onClick={() => handleSave("Tutorial 12:00 - 14:00")}
+            >
+              Select
+            </button>
+          </div>
+          <div className="flex justify-between border-b-2">
+            <span className="m-8">Tutorial 15:00 - 17:00</span>
+            <button
+              className="ring-2 ring-gray-300 hover:bg-gray-100 rounded-2xl float-right py-2 px-10 m-6"
+              onClick={() => handleSave("Tutorial 15:00 - 17:00")}
+            >
+              Select
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Select;
