cc1e266e49dbbe4332967c0cadf3f695a8d8ebe8
KhoaNguyenAn <nguyenankhoa1402@gmail.com>
2023-12-09 02:01:40 +1100

    implement function to import data from firebase in order to display timetable

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, EventSettingsModel } from '@syncfusion/ej2-react-schedule';
 import { registerLicense } from '@syncfusion/ej2-base';
-import React from 'react';
+import React, { useEffect, useState, useLayoutEffect } from "react";
+import { db } from "../firebase-handler";
+import { doc, collection, getDoc } from "firebase/firestore";
+import { auth } from "../firebase-handler";
 import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
 
+
 registerLicense('Ngo9BigBOggjHTQxAR8/V1NHaF1cWWhIYVZpR2Nbe05zfldCal9UVAciSV9jS31SdEVlWXxcdHdTRWdaUg==');
+
 const localData = {
     dataSource: [
         {
@@ -21,14 +26,79 @@ const localData = {
             EndTime: new Date(2023, 11, 5, 13, 30),
             StartTime: new Date(2023, 11, 5, 11, 0)
         },
-
     ]
 }
 
 const Home = () => {
+    const [selectedTimeslots, setSelectedTimeslots] = useState({});
+    let timeTableDataAllocated = [];
+    
+    useLayoutEffect(() => {
+        const loadSelectedTimeslotsFromFirestore = async () => {
+            const user = auth.currentUser;
+
+            if (user && user.uid) {
+                const userId = user.uid;
+                const usersRef = collection(db, "users");
+                const userDocRef = doc(usersRef, userId);
+
+                try {
+                    const userDocSnapshot = await getDoc(userDocRef);
+
+                    if (userDocSnapshot.exists()) {
+                        const userSelectedTimeslots = userDocSnapshot.data().slots || [];
+                        const selectedTimeslotsData = {};
+                        userSelectedTimeslots.forEach((slot) => {
+                            const { unit, timeSlot } = slot;
+                            selectedTimeslotsData[unit] = [...(selectedTimeslotsData[unit] || []), timeSlot];
+                        });
+
+                        setSelectedTimeslots(selectedTimeslotsData);
+                    }
+                } catch (error) {
+                    console.error("Error loading selected timeslots:", error);
+                }
+            }
+        };
+
+        loadSelectedTimeslotsFromFirestore();
+    }, []);
+
+    useEffect(() => {
+        processSelectedTimeslots();
+    }, [selectedTimeslots]);
+
+    const processSelectedTimeslots = () => {
+        for (const unit in selectedTimeslots) {
+            if (selectedTimeslots.hasOwnProperty(unit)) {
+                const timeSlots = selectedTimeslots[unit];
+                const convertedData = parseTimeSlots(timeSlots, unit);
+                timeTableDataAllocated = timeTableDataAllocated.concat(convertedData);
+                console.log(timeTableDataAllocated)
+            }
+        }
+    };
+
+    const parseTimeSlots = (timeSlots, unit) => {
+        return timeSlots.map((timeSlot) => {
+            const [subject, timeRange] = timeSlot.split(' ');
+            const [startTime, endTime] = timeRange.split(' - ');
+            const fullSubject = `${unit} - ${subject}`;
+
+            const startDateTime = new Date(`2023-11-01 ${startTime}`);
+            const endDateTime = new Date(`2023-11-01 ${endTime}`);
+
+            return {
+                Subject: fullSubject,
+                StartTime: startDateTime,
+                EndTime: endDateTime,
+            };
+        });
+    };
+
     return (
         <div>
-            <div class=" bg-white dark:bg-slate-800 shadow-xl overflow-hidden">
+            <div className="bg-white dark:bg-slate-800 shadow-xl overflow-hidden">
                 <header className="bg-black text-white text-center font-serif text-3xl py-6 border-b border-white dark:border-slate-800">
                     Time Table Monash
                 </header>
@@ -41,7 +111,7 @@ const Home = () => {
                     </ul>
                 </nav>
             </div>
-            <ScheduleComponent eventSettings={localData}>
+            <ScheduleComponent eventSettings={{ dataSource: timeTableDataAllocated }}>
                 <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
             </ScheduleComponent>
         </div>
