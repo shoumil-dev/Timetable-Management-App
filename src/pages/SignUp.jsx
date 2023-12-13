a25268c29c5963a539adef3102c71e2d2ff6039c
Mhar0027 <mhar0027@student.monash.edu>
2023-12-14 01:16:37 +0800

    Update SignUp.jsx
    Changes:
    -Created user prompt for application rights (admin/student/lecturer) *subject to changes
    -Storage of status in Firestore database

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,19 +1,33 @@
 import { auth } from "../firebase-handler";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
+import { setDoc, doc, collection } from "firebase/firestore";
+import { db } from "../firebase-handler";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [status, setStatus] = useState(""); // Add status state for admin/lecturer/student
   const [error, setError] = useState(null);
 
   const signUp = (e) => {
     e.preventDefault();
+
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        console.log("User signed up:", userCredential.user);
+        const userId = userCredential.user.uid;
 
+        // Store user information in Firestore
+        const usersRef = collection(db, "users");
+        setDoc(doc(usersRef, userId), {
+          userId,
+          email,
+          status, // Include user status
+          slots: [],
+        });
+
+        console.log("User signed up:", userCredential.user);
       })
       .catch((error) => {
         console.error("Sign-up error:", error);
@@ -76,6 +90,29 @@ const SignUp = () => {
           {error && (
             <div className="text-red-500">{error}</div>
           )}
+
+<div>
+          <label
+            htmlFor="status"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            User Status
+          </label>
+          <div className="mt-2">
+            <select
+              id="status"
+              name="status"
+              value={status}
+              onChange={(e) => setStatus(e.target.value)}
+              className="block w-full rounded-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-gray-900 sm:text-sm sm:leading-6"
+            >
+              <option value="">Select status</option>
+              <option value="admin">Admin</option>
+              <option value="lecturer">Lecturer</option>
+              <option value="student">Student</option>
+            </select>
+          </div>
+        </div>
 
           <div className="flex items-center justify-center">
             <button
