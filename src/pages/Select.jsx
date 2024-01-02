e4cee7ef15a98806c525db53d9b1bf8a7d5771e6
sguh0003 <sguh0003@student.monash.edu>
2024-01-02 22:25:36 +0800

    Dark mode select.jsx testing

--- src/pages/Select.jsx
+++ src/pages/Select.jsx
@@ -27,31 +27,31 @@ const Select = () => {
     // Load user-specific selected timeslots from Firestore on component mount
     const loadSelectedTimeslotsFromFirestore = async () => {
       const user = auth.currentUser;
-  
+
       // Check if the user is authenticated before accessing uid
       if (user && user.uid) {
         const userId = user.uid;
         const usersRef = collection(db, "users");
         const userDocRef = doc(usersRef, userId);
         const userDocSnapshot = await getDoc(userDocRef);
-  
+
         if (userDocSnapshot.exists()) {
           const userSelectedTimeslots = userDocSnapshot.data().slots || [];
           const selectedTimeslotsData = {};
-  
+
           userSelectedTimeslots.forEach((slot) => {
             const { unit, timeSlot } = slot;
             selectedTimeslotsData[unit] = [...(selectedTimeslotsData[unit] || []), timeSlot];
           });
-  
+
           setSelectedTimeslots(selectedTimeslotsData);
         }
       }
     };
-  
+
     loadSelectedTimeslotsFromFirestore();
   }, [selectedUnit]);
-  
+
 
   const handleUnitClick = async (unit) => {
     setSelectedUnit(unit);
@@ -106,44 +106,44 @@ const Select = () => {
   const handleDeselectButtonClick = async (timeSlot) => {
     if (selectedUnit && timeSlot) {
       const userId = auth.currentUser.uid;
-  
+
       const usersRef = collection(db, "users");
       const userDocRef = doc(usersRef, userId);
       const userDocSnapshot = await getDoc(userDocRef);
-  
+
       if (userDocSnapshot.exists()) {
         const existingSlots = userDocSnapshot.data().slots || [];
         const isAlreadySelected = existingSlots.some(
           (slot) => slot.unit === selectedUnit && slot.timeSlot === timeSlot
         );
-  
+
         const confirmed = window.confirm(
           `You are about to ${isAlreadySelected ? "deselect" : "select"} ${timeSlot} for ${selectedUnit}. Do you want to proceed?`
         );
-  
+
         if (!confirmed) {
           return;
         }
-  
+
         const updatedSlots = existingSlots.filter(
           (slot) => !(slot.unit === selectedUnit && slot.timeSlot === timeSlot)
         );
-  
+
         await updateDoc(userDocRef, {
           slots: updatedSlots,
         });
-  
+
         setSelectedTimeslots((prevSelectedTimeslots) => ({
           ...prevSelectedTimeslots,
           [selectedUnit]: updatedSlots.map((slot) => slot.timeSlot),
         }));
-  
+
         console.log(`Successfully ${isAlreadySelected ? "removed" : "added"} timeslot`);
       }
     }
   };
-  
-  
+
+
   return (
     <div className="bg-white shadow-xl overflow-hidden">
       <header className="bg-black text-white text-center font-serif text-3xl py-6 border-b border-white">
@@ -151,41 +151,40 @@ const Select = () => {
       </header>
       <nav className="bg-black text-white p-4">
         <ul className="flex space-x-4">
-          <li><a href="http://localhost:3000/User" className="hover:text-gray-400"><FontAwesomeIcon icon={faUser} /></a></li>
+          <li><a href="http://localhost:3000/User" className="hover:text-zinc-400"><FontAwesomeIcon icon={faUser} /></a></li>
           <li>
-            <a href="http://localhost:3000/Home" className="hover:text-gray-400">
+            <a href="http://localhost:3000/Home" className="hover:text-zinc-400">
               Home
             </a>
           </li>
           <li>
-            <a href="http://localhost:3000/Create" className="hover:text-gray-400">
+            <a href="http://localhost:3000/Create" className="hover:text-zinc-400">
               Create Unit
             </a>
           </li>
           <li>
             <a
               href="http://localhost:3000/Select"
-              className="hover:text-gray-400 bg-blue-500 text-white hover:bg-blue-600 p-4"
+              className="hover:text-zinc-400 bg-blue-500 text-white hover:bg-blue-600 p-4"
             >
               Timeslot allocation
             </a>
           </li>
           <li className="ml-auto">
-            <a href="http://localhost:3000/" className="hover:text-gray-400">
+            <a href="http://localhost:3000/" className="hover:text-zinc-400">
               Log Out
             </a>
           </li>
         </ul>
       </nav>
 
-      <div className="flex h-screen">
-        <div className="overflow-auto ring-2 ring-gray-300 w-1/5 rounded-2xl text-xl mr-4 m-20 text-center">
+      <div className="flex h-screen dark: bg-zinc-900 text-white">
+        <div className="overflow-auto ring-2 ring-zinc-300 w-1/5 rounded-2xl text-xl mr-4 m-20 text-center">
           {units.map((unit) => (
             <div
               key={unit.title}
-              className={`p-8 hover:bg-black hover:text-white border-b-2 ${
-                selectedUnit && selectedUnit === unit.title ? "bg-black text-white" : ""
-              }`}
+              className={`p-8 hover:bg-black dark:hover:bg-zinc-700 hover:text-white border-b-2 ${selectedUnit && selectedUnit === unit.title ? "bg-zinc-700 text-white" : ""
+                }`}
             >
               <button onClick={() => handleUnitClick(unit.title)}>
                 {unit.title}
@@ -194,35 +193,34 @@ const Select = () => {
           ))}
         </div>
 
-        <div className="overflow-auto ring-2 ring-gray-300 w-4/5 rounded-2xl text-xl ml-4 m-20">
-  {timeSlots.map((timeSlot, index) => (
-    <div key={index} className="flex justify-between border-b-2">
-      <span className="m-8">{timeSlot}</span>
-      <div className="flex items-center">
-      <button
-  className={`ring-2 ring-gray-300 ${
-    selectedTimeslots[selectedUnit]?.includes(timeSlot)
-      ? "bg-gray-700 text-white"
-      : "hover:bg-gray-100"
-  } rounded-full py-2 px-6 m-2 transition duration-300`}
-  onClick={() => handleSelectButtonClick(timeSlot)}
-  disabled={selectedTimeslots[selectedUnit]?.includes(timeSlot)}
->
-  {selectedTimeslots[selectedUnit]?.includes(timeSlot) ? "Selected" : "Select"}
-</button>
+        <div className="overflow-auto ring-2 ring-zinc-300 w-4/5 rounded-2xl text-xl ml-4 m-20">
+          {timeSlots.map((timeSlot, index) => (
+            <div key={index} className="flex justify-between border-b-2">
+              <span className="m-8">{timeSlot}</span>
+              <div className="flex items-center">
+                <button
+                  className={`ring-2 ring-zinc-300 ${selectedTimeslots[selectedUnit]?.includes(timeSlot)
+                      ? "bg-zinc-700 text-white"
+                      : "hover:bg-zinc-100 dark:hover:bg-zinc-700"
+                    } rounded-full py-2 px-6 m-2 transition duration-300`}
+                  onClick={() => handleSelectButtonClick(timeSlot)}
+                  disabled={selectedTimeslots[selectedUnit]?.includes(timeSlot)}
+                >
+                  {selectedTimeslots[selectedUnit]?.includes(timeSlot) ? "Selected" : "Select"}
+                </button>
 
-{selectedTimeslots[selectedUnit]?.includes(timeSlot) && (
-  <button
-    className="ml-2 mr-2 bg-red-500 text-white rounded-full py-2 px-4 hover:bg-red-600 transition duration-300"
-    onClick={() => handleDeselectButtonClick(timeSlot)}
-  >
-    Deselect
-  </button>
-)}
-      </div>
-    </div>
-  ))}
-</div>
+                {selectedTimeslots[selectedUnit]?.includes(timeSlot) && (
+                  <button
+                    className="ml-2 mr-2 bg-red-500 text-white rounded-full py-2 px-4 hover:bg-red-600 transition duration-300"
+                    onClick={() => handleDeselectButtonClick(timeSlot)}
+                  >
+                    Deselect
+                  </button>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
